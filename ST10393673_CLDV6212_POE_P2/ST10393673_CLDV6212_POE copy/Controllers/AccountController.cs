dd8263d46de0b6a61b0487207baba8686f2948e7
MathewMizrachi <mgmizrachi@gmail.com>
2024-10-01 22:15:39 +0200

    Included all Code for Functions as requested
    Restored Github glitch and managed to upload all files pertaining to part 2 of the poe

---
+++ ST10393673_CLDV6212_POE_P2/ST10393673_CLDV6212_POE copy/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10393673_CLDV6212_POE.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string username, string email, string password)
+        {
+            // Handle registration logic here
+            // e.g., save user to database, validate input, etc.
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
